2d59bbcbdeaea8ab3c2ec83cf7ebde89d92c897e
weiyinhua <2715478311@qq.com>
2019-05-16 14:43:13 +0800

    基础学习01

--- test_platform/personal/views.py
+++ test_platform/personal/views.py
@@ -3,5 +3,5 @@ from django.shortcuts import render
 
 # Create your views here.
 def say_hello(request):
-    name = request.GET.get("name",)
+    name = request.GET.get("name1","")#获取（get）,GET请求(http://127.0.0.1:8000/hello/?name1=tom1)
     return HttpResponse("Hello, " + name)
