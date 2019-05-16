9ce2875f4c251d456a7522b64132481a7f946dbd
weiyinhua <2715478311@qq.com>
2019-05-16 17:34:41 +0800

    django基础学习之MTV模式

--- test_platform/personal/views.py
+++ test_platform/personal/views.py
@@ -3,5 +3,15 @@ from django.shortcuts import render
 
 # Create your views here.
 def say_hello(request):
-    name = request.GET.get("name1","")#获取（get）,GET请求(http://127.0.0.1:8000/hello/?name1=tom1)
-    return HttpResponse("Hello, " + name)
+    """
+    :param request:
+    :return:
+    """
+
+    input_name = request.GET.get("name","")#获取（get）,GET请求(http://127.0.0.1:8000/hello/?name=tom1)
+    if input_name=="":
+        return HttpResponse("Please input 'http://127.0.0.1:8000/hello/?name=name1'")
+
+    #return HttpResponse("Hello, " + name)
+
+    return render(request, "index.html",{"get_name":input_name})
