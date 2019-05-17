2519832d308b0a548c1106a373435a3bf2d408b2
weiyinhua <2715478311@qq.com>
2019-05-17 15:20:48 +0800

    django实现登录

--- test_platform/personal/views.py
+++ test_platform/personal/views.py
@@ -13,5 +13,22 @@ def say_hello(request):
         return HttpResponse("Please input 'http://127.0.0.1:8000/hello/?name=name1'")
 
     #return HttpResponse("Hello, " + name)
-
     return render(request, "index.html",{"get_name":input_name})
+
+def index(request):
+    """
+    登录页面
+    :param request:
+    :return:
+    """
+    method = request.method
+    if method=="GET":
+        return render(request, "index.html")
+    else:
+        input_username = request.POST.get("username", "")
+        input_password = request.POST.get("password", "")
+        if input_username != "tom" or input_password != "123":
+            # return HttpResponse("Error entering username or password")
+            return render(request, "index.html", {"error": "Error entering username or password"})
+
+        return HttpResponse("ok")
